449664494f336a2b7a509972ce13f8d7b9687c44
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 21:26:06 +0530

    Create Question-04.C

---
+++ Question-04.C
@@ -0,0 +1,18 @@
+// Write a program to calculate sum of squares of first N natural numbers.
+
+#include<stdio.h>
+int main()
+{
+    int x = 1 , n , sum = 0 ;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    while (x <= n)
+    {
+        sum = sum + x*x ;
+        x++;
+    }
+    printf("\nSum of first %dth natural no.s is %d: ",n,sum);
+
+    return 0;
+}
